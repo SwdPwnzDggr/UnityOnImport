c268c56e267e55f8993f1b4c4e5a87682e34063c
Justin Schwieger <Justin.SMF@gmail.com>
2018-02-10 10:43:25 -0800

    Pusing up initial Unity Project, base of OnImport created.

---
+++ OnImport/Assets/Editor/OnImportWindow.cs
@@ -0,0 +1,130 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class OnImport : EditorWindow
+{
+    string scriptName;
+    string directoryName;
+    bool groupEnabled;
+    bool myBool = true;
+    float myFloat = 1.23f;
+
+    public enum AssetType { NotSelected,Audio,Material,Model,Texture}
+    AssetType assetType = AssetType.NotSelected;
+
+    enum TextureType { Default, NormalMap, EditorGUIAndLegacyGUI, Sprite, Cursor, Cookie, Lightmap, SingleChannel };
+
+
+
+    // Add menu item named "My Window" to the Window menu
+    [MenuItem("Window/On Import")]
+    public static void ShowWindow()
+    {
+        //Show existing window instance. If one doesn't exist, make one.
+        EditorWindow.GetWindow(typeof(OnImport));
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Base Settings", EditorStyles.boldLabel);
+        scriptName = EditorGUILayout.TextField("Class Name", scriptName);
+        directoryName = EditorGUILayout.TextField("Directory Name", directoryName);
+        assetType = (AssetType)EditorGUILayout.EnumPopup("Asset Type",assetType);
+        switch(assetType)
+        {
+            case (AssetType.NotSelected):
+                return;
+            case (AssetType.Audio):
+                DisplayAudioOptions();
+                break;
+            case (AssetType.Material):
+                DisplayMaterialOptions();
+                break;
+            case (AssetType.Model):
+                DisplayModelOptions();
+                break;
+            case (AssetType.Texture):
+                DisplayTextureOptions();
+                break;
+            default:
+                Debug.LogError("Somehow there is no type Selected on OnImport");
+                break;
+        }
+    }
+
+    void DisplayAudioOptions()
+    {
+        GUILayout.Label("Audio Import Settings", EditorStyles.boldLabel);
+
+        if (GUILayout.Button("Generate"))
+        {
+            CreateScript();
+        }
+    }
+
+    void DisplayTextureOptions()
+    {
+        GUILayout.Label("Texture Import Settings", EditorStyles.boldLabel);
+        GUILayout.Label("Base Settings", EditorStyles.miniLabel);
+        TextureType type = TextureType.Default;
+        type = (TextureType)EditorGUILayout.EnumPopup("Texture Type", type); ;
+        if (GUILayout.Button("Generate"))
+        {
+            CreateScript();
+        }
+    }
+    void DisplayMaterialOptions()
+    {
+        GUILayout.Label("Material Import Settings", EditorStyles.boldLabel);
+
+        if (GUILayout.Button("Generate"))
+        {
+            CreateScript();
+        }
+    }
+    void DisplayModelOptions()
+    {
+        GUILayout.Label("Model Import Settings", EditorStyles.boldLabel);
+
+        if (GUILayout.Button("Generate"))
+        {
+            CreateScript();
+        }
+    }
+
+    void CreateScript()
+    {
+        string name = scriptName.Replace(" ", "_");
+        name = name.Replace("-", "_");
+        string copyPath = "Assets/Editor/" + name + ".cs";
+        Debug.Log("Creating Classfile: " + copyPath);
+        //if (File.Exists(copyPath) == false)
+      //  { // do not overwrite
+            using (StreamWriter outfile = new StreamWriter(copyPath))
+            {
+                outfile.WriteLine("using UnityEngine;");
+                outfile.WriteLine("using System.Collections;");
+                outfile.WriteLine("using UnityEditor;");
+                outfile.WriteLine("");
+                outfile.WriteLine("public class " + name + " : AssetPostprocessor ");
+                outfile.WriteLine(" {");
+                outfile.WriteLine("     ");
+                outfile.WriteLine("     // Use this for initialization");
+                outfile.WriteLine("     void OnPostprocessTexture (Texture2D texture)");
+                outfile.WriteLine("     {");
+                outfile.WriteLine("         string lowerCaseAssetPath = assetPath.ToLower ();");
+                outfile.WriteLine("         bool isInDirectory = lowerCaseAssetPath.IndexOf (\"/"+ directoryName.ToString() + "/\") != -1;");
+                outfile.WriteLine("         ");
+                outfile.WriteLine("         if (isInDirectory) ");
+                outfile.WriteLine("         {");
+                outfile.WriteLine("             TextureImporter textureImporter = (TextureImporter) assetImporter;");
+                outfile.WriteLine("             textureImporter.textureType = TextureImporterType.Sprite;");
+                outfile.WriteLine("         }");
+                outfile.WriteLine("     }");
+                outfile.WriteLine(" }");
+          //  }//File written
+        }
+        AssetDatabase.Refresh();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # UnityOnImport
 Allows you to easily create new import settings for specific folders
+
+Created in Unity 5.5
